c9761a4c89719e840786019e4e4dfbdca4d3ad6a
David Sword <vexiconlieus@gmail.com>
2023-04-21 17:05:25 -0700

    Added routing to connect express and react

--- app.js
+++ app.js
@@ -7,4 +7,12 @@ const app = express();
 
 app.listen(port, () => {console.log(`Listening on ${port}`)});
 
-app.use(express.static(path.resolve(__dirname, '../project/build')));
+app.use(express.static(path.resolve(__dirname, './project/build')));
+
+app.get("/api-docs", (req, res) => {
+    res.json({ message: "API-DOC View" });
+  });
+
+app.get('home', (req, res) => {
+res.sendFile(path.resolve(__dirname, './project/public', 'index.html'));
+});
