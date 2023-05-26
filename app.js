d9d3f266d73e360f615735d16449408f1f3bd567
David <vexiconlieus@gmail.com>
2023-05-26 06:38:35 -0700

    Added server data validation & client side resp

--- app.js
+++ app.js
@@ -30,11 +30,64 @@ app.get("/customer/:id", async (req, res) => {
   res.status(200).json(filtered);
 });
 
+const checkEmpty = function(obj, optionalKeys){
+  var ret = true;
+  Object.keys(obj).forEach(key => {
+    if(obj[key] === '' && !optionalKeys.includes(key)){
+      console.log(`[${key}] is empty`);
+      ret = false;
+    }
+  })
+  return ret;
+}
+
+const checkKeys = function(obj, keys){
+  const keyArray = Object.keys(obj);
+  if(keyArray.length() !== keys.length()){
+    return false;
+  }
+  keys.forEach(key => {
+    if(!keyArray.includes(key)){
+      return false;
+    }
+  });
+  return true;
+}
+
+const validateFloat = function(obj, keys){
+  keys.forEach(key => {
+    try{
+      if(obj[key] === ""){ //ice2 optional case
+        return true;
+      }
+      if(parseFloat(obj[key]) !== NaN){
+        return true;
+      }
+    }catch(err){
+      return false;
+    }
+  })
+}
+
 app.post("/customer/", async (req, res) => {
-  console.log(req.body);
-  res.sendStatus(200);
-  /*const _db = await mongo.connect().db('ice').collection("customers");
-  _db.insertOne(req.body).then(result => res.sendStatus(201)).catch(err => res.sendStatus(400));*/
+  try{
+    
+    console.log(req.body);
+    const checkEmptyResponse = checkEmpty(req.body, ["ice2", "price2", "rami", "equipment", "job", "po"]);
+    console.log(checkEmptyResponse);
+    if(checkEmptyResponse === false){
+      console.log("err");
+      throw Error("Empty response");
+      
+    }
+    res.sendStatus(200);
+    /*const _db = await mongo.connect().db('ice').collection("customers");
+  _ db.insertOne(req.body).then(result => res.sendStatus(201)).catch(err => res.sendStatus(400));*/
+  }catch(err){
+    console.log(err)
+    res.sendStatus(400);
+  }
+  
 })
 
 mongo.initialize();
