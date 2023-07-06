1df302bea3721cc9075110ed1529dd92101392c0
David Sword <vexiconlieus@gmail.com>
2023-07-05 18:09:26 -0700

    Replaced header and functions from router to form

--- mac-app/src/pages/customerForm.js
+++ mac-app/src/pages/customerForm.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import { useNavigate } from 'react-router-dom';
 import '../styles/form.css';
 import {getJWT} from '../utility';
+import FormHeader from '../components/formHeader';
 
 function CustomerForm(props){
     const nav = useNavigate();
@@ -21,7 +22,8 @@ function CustomerForm(props){
         'equipment': '', 
         'special': '',
     })
-    const [rep, setRep] = useState(null);
+    const [edit, setEdit] = useState(props.method === "PUT" ? true : false);
+    const [response, setResponse] = useState({});
     async function updateData(){
         var link = props.api;
         if(props._id){
@@ -35,65 +37,94 @@ function CustomerForm(props){
             credentials: 'include'
         }).then(response => {
             if(response.ok){
-                setRep(true);
                 if(props._id){
                     window.location.reload(true);
-                    props.response(`Customer successfully ${props.method === 'POST' ? 'added' : 'updated'}.`);
+                    setResponse({text: `Customer successfully ${props.method === 'POST' ? 'added' : 'updated'}.`, status: true});
                 }else{
-                    nav("/customer/")
+                    setResponse({text: `Customer successfully ${props.method === 'POST' ? 'added' : 'updated'}.`, status: true});
+                    setTimeout(() => {
+                        nav("/customer/")
+                    }, 3000);
                 }
             }else{
-                setRep(false);
-                props.response(`Unable to ${props.method === 'POST' ? 'add' : 'update'} customer due to an error.`);
+                setResponse({text: `Unable to ${props.method === 'POST' ? 'add' : 'update'} customer due to an error.`, status: false});
             }
-        }).catch(err => {console.log(err); setRep(false);});
+        }).catch(err => {console.log("err")});
         
     }
     function handleSubmit(e){
         e.preventDefault();  
         updateData();
     }
+    async function deleteCustomer(){
+        await fetch(props.api + props._id, {
+            method: 'DELETE',withCredentials: true, headers: {'Authorization': `Bearer ${token}`}}
+        ).then(response => {
+            if(response.ok){
+                setResponse({text: `Customer successfully deleted.`, status: true});
+                setTimeout(() => {
+                    nav("/customer/")
+                }, 1500);
+            }else{
+                setResponse({text: `Unable to delete customer due to an error.`, status: false});
+            }
+        }).catch(err => {setResponse({text: `Unable to delete customer due to an error: ${err}.`, status: false});});
+    }
+    function handleDelete(e){
+        e.preventDefault();
+        deleteCustomer()
+    }
     useEffect(() => {
         if(props._id && data.name === ""){
             fetch(props.api + props._id, {withCredentials: true, headers: {'Authorization': `Bearer ${token}`}}).then(response => response.json()).then(obj => {setData(obj[0]); console.log(obj)});
         } 
     }, []);
-       
+    const toggle = () => {
+        if(edit){
+            setEdit(false);
+        }else{
+            setEdit(true);
+        }
+    }
     return (<>
-        {rep === true && <h1>Successfully updated customer</h1>}
-        {rep === false && <h1>Failed to add customer, please try again</h1>}
+        <FormHeader 
+            title={props.method === 'POST' ? 'New Customer' : 'Customer Details'} 
+            response={response} 
+            toggle={toggle}
+            delete={handleDelete}
+            method={props.method}/>
         <form className='form' onSubmit={handleSubmit}>
-            <input type="text" required className='form-text-input' name="name" placeholder='Customer Name' value={data.name || ""} readOnly={props._edit} onChange={e => setData({...data, ["name"] : e.target.value})} />
-            <input type="text" required className='form-text-input' name="address" placeholder='Street Address' value={data.address || ""} readOnly={props._edit} onChange={e => setData({...data, ["address"] : e.target.value})}/>
-            <input type="text" required className='form-text-input' name="city" placeholder='City' value={data.city || ""} readOnly={props._edit} onChange={e => setData({...data, ["city"] : e.target.value})}/>
-            <input type="text" required className='form-text-input' name="zip" placeholder='Zip Code' value={data.zip || ""} readOnly={props._edit} onChange={e => setData({...data, ["zip"] : e.target.value})}/>
+            <input type="text" required className='form-text-input' name="name" placeholder='Customer Name' value={data.name || ""} readOnly={edit} onChange={e => setData({...data, ["name"] : e.target.value})} />
+            <input type="text" required className='form-text-input' name="address" placeholder='Street Address' value={data.address || ""} readOnly={edit} onChange={e => setData({...data, ["address"] : e.target.value})}/>
+            <input type="text" required className='form-text-input' name="city" placeholder='City' value={data.city || ""} readOnly={edit} onChange={e => setData({...data, ["city"] : e.target.value})}/>
+            <input type="text" required className='form-text-input' name="zip" placeholder='Zip Code' value={data.zip || ""} readOnly={edit} onChange={e => setData({...data, ["zip"] : e.target.value})}/>
             
-            <select name="ice1" required className='form-select-input' value={data.ice1 || "0"} disabled={props._edit} onChange={e => setData({...data, ["ice1"] : e.target.value})}>
+            <select name="ice1" required className='form-select-input' value={data.ice1 || "0"} disabled={edit} onChange={e => setData({...data, ["ice1"] : e.target.value})}>
                 {(data.ice1 == 0 ? <option value="0" selected></option> : <option value="0"></option>)}
                 {(data.ice1 == 7 ? <option value="7" selected>7R</option> : <option value="7">7R</option>)}
                 {(data.ice1 == 10 ? <option value="10" selected>10lb block</option> : <option value="10">10lb block</option>)}
                 {(data.ice1 == 16 ? <option value="16" selected>16R</option> : <option value="16">16R</option>)}
                 {(data.ice1 == 40 ? <option value="40" selected>40R</option> : <option value="40">40R</option>)}
             </select>
-            <input type="number" required className='form-text-input' name="price1" placeholder='Ice Price' step=".01" readOnly={props._edit} value={data.price1 || ""} onChange={e => setData({...data, ["price1"] : e.target.value})}/>
-            <select name="ice2" className='form-select-input' disabled={props._edit} onChange={e => setData({...data, ["ice2"] : e.target.value})}>
+            <input type="number" required className='form-text-input' name="price1" placeholder='Ice Price' step=".01" readOnly={edit} value={data.price1 || ""} onChange={e => setData({...data, ["price1"] : e.target.value})}/>
+            <select name="ice2" className='form-select-input' disabled={edit} onChange={e => setData({...data, ["ice2"] : e.target.value})}>
                 {(data.ice2 == 0 ? <option value="0" selected></option> : <option value="0"></option>)}
                 {(data.ice2 == 7 ? <option value="7" selected>7R</option> : <option value="7">7R</option>)}
                 {(data.ice2 == 10 ? <option value="10" selected>10lb block</option> : <option value="10">10lb block</option>)}
                 {(data.ice2 == 16 ? <option value="16" selected>16R</option> : <option value="16">16R</option>)}
                 {(data.ice2 == 40 ? <option value="40" selected>40R</option> : <option value="40">40R</option>)}
             </select>
-            <input type="number" className='form-text-input' name="price2" placeholder='Ice Price' step=".01" readOnly={props._edit} value={data.price2 || ""} onChange={e => setData({...data, ["price2"] : e.target.value})}/>
+            <input type="number" className='form-text-input' name="price2" placeholder='Ice Price' step=".01" readOnly={edit} value={data.price2 || ""} onChange={e => setData({...data, ["price2"] : e.target.value})}/>
             <label htmlFor='tax' className='form-check-label'>Tax</label>
-            <input type="checkbox" className='form-check-input' name="tax" checked={data.tax || false} disabled={props._edit} onChange={e => setData({...data, ["tax"] : e.target.checked})}/>
+            <input type="checkbox" className='form-check-input' name="tax" checked={data.tax || false} disabled={edit} onChange={e => setData({...data, ["tax"] : e.target.checked})}/>
             <label htmlFor='delivery' className='form-check-label'>Delivery</label>
-            <input type="checkbox" className='form-check-input' name="delivery" checked={data.delivery || false} disabled={props._edit} onChange={e => setData({...data, ["delivery"] : e.target.checked})}/>
-            <input type="text" className='form-text-input' name="po" placeholder='PO' readOnly={props._edit} value={data.po || ""}  onChange={e => setData({...data, ["po"] : e.target.value})}/>
-            <input type="text" className='form-text-input' name="job" placeholder='Job #' readOnly={props._edit} value={data.job || ""}  onChange={e => setData({...data, ["job"] : e.target.value})}/>
-            <input type="text" className='form-text-input' name="rami" placeholder='RAMI' readOnly={props._edit} value={data.rami || ""}  onChange={e => setData({...data, ["rami"] : e.target.value})}/>
-            <input type="text" className='form-text-input' name="equipment" placeholder='Box Type' readOnly={props._edit} value={data.equipment || ""}   onChange={e => setData({...data, ["equipment"] : e.target.value})}/>
-            <input type="text" className='form-text-input' name="special" placeholder='Special Instructions' readOnly={props._edit} onChange={e => setData({...data, ["special"] : e.target.value})}/>
-            {!props._edit && <button type="submit" className='form-button-submit'>Submit</button>}
+            <input type="checkbox" className='form-check-input' name="delivery" checked={data.delivery || false} disabled={edit} onChange={e => setData({...data, ["delivery"] : e.target.checked})}/>
+            <input type="text" className='form-text-input' name="po" placeholder='PO' readOnly={edit} value={data.po || ""}  onChange={e => setData({...data, ["po"] : e.target.value})}/>
+            <input type="text" className='form-text-input' name="job" placeholder='Job #' readOnly={edit} value={data.job || ""}  onChange={e => setData({...data, ["job"] : e.target.value})}/>
+            <input type="text" className='form-text-input' name="rami" placeholder='RAMI' readOnly={edit} value={data.rami || ""}  onChange={e => setData({...data, ["rami"] : e.target.value})}/>
+            <input type="text" className='form-text-input' name="equipment" placeholder='Box Type' readOnly={edit} value={data.equipment || ""}   onChange={e => setData({...data, ["equipment"] : e.target.value})}/>
+            <input type="text" className='form-text-input' name="special" placeholder='Special Instructions' readOnly={edit} onChange={e => setData({...data, ["special"] : e.target.value})}/>
+            {!edit && <button type="submit" className='form-button-submit'>Submit</button>}
         </form>
         
     </>)
--- mac-app/src/pages/customerRouter.js
+++ mac-app/src/pages/customerRouter.js
@@ -6,35 +6,7 @@ import CustomerHistory from "../components/customerHistory";
 
 function CustomerRouter(){
     const parameters = useParams();
-    const nav = useNavigate();
-    const [edit, setEdit] = useState(true);
-    const [response, setResponse] = useState("");
-    const [confirm, setConfirm] = useState(false);
 
-    function handleResponse(message){
-        setResponse(message);
-        setTimeout(()=>{setResponse("")}, 1000);
-    }
-    const toggle = () => {
-        if(edit){
-            setEdit(false);
-        }else{
-            setEdit(true);
-        }
-    }
-
-    async function deleteCustomer(){
-        await fetch("https://cit-490-ice.onrender.com/customer/" + parameters['*'].split("/")[1], {
-            method: 'DELETE'}
-        ).then(response => {
-            if(response.ok){
-                
-                nav("/customer/");
-            }else{
-                console.log("failed");
-            }
-        }).catch(err => {console.log(err); console.log("failed");});
-    }
     return (
     <Routes>
         
@@ -44,7 +16,6 @@ function CustomerRouter(){
                 <h1 className="form-title">Customers</h1>
                 <Link className="table-link-create" to="./add/">New</Link>
             </div>
-            {response !== "" && <p className='form-status-message'>{response}</p>}
             <MyTableView 
                 header_keys={["Name", "Address", "Ice", "Price", "Tax", "Del", "PO/Job"]}
                 api={"http://localhost:4000/customer/"}
@@ -52,26 +23,15 @@ function CustomerRouter(){
                 model="customer"/>
         </>}/>
         <Route path="/add/" element={<>
-            <div className="form-action-header">
-                <h1 className="form-title">New Customer</h1>
-            </div>
-        <CustomerForm method={"POST"} edit={true} api="http://localhost:4000/customer/" response={handleResponse}/>
+        <CustomerForm method={"POST"} api="http://localhost:4000/customer/"/>
         </>}/>
         <Route path="/delete/:id" element={<h1>Customer Delete View: {parameters['*'].split("/")[1]}</h1>}/>
         <Route path="/edit/:id" element={
-            <>
-            <div className="form-action-header">
-                <h1 className="form-title">Customer Details</h1>
-                <button type="button" className="form-button-edit" onClick={toggle}>Edit</button>
-                <button type="button" className="form-button-delete" onClick={e => setConfirm(true)}>Delete</button>
-                {confirm && <>
-                <h1 className="form-title">Are you sure about deleting this customer?</h1>
-                <button className="form-button-delete" onClick={deleteCustomer}>Yes</button>
-                <button className="form-button-edit" onClick={e => setConfirm(false)}>No</button>
-            </>}
-            </div>
-            
-            <CustomerForm _id={parameters['*'].split("/")[1]} _edit={edit} method="PUT" api="http://localhost:4000/customer/" response={handleResponse}/>
+            <> 
+            <CustomerForm 
+                _id={parameters['*'].split("/")[1]} 
+                method="PUT" 
+                api="http://localhost:4000/customer/"/>
 
             <CustomerHistory api="http://localhost:4000/customer/history/" _id={parameters['*'].split("/")[1]}/></>
         }/>
