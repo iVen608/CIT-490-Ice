daaa7b68e5693d2573b358a0c852f85308b8cf10
David Sword <vexiconlieus@gmail.com>
2023-07-05 18:09:39 -0700

    Added header styling to login

--- mac-app/src/pages/login.js
+++ mac-app/src/pages/login.js
@@ -4,9 +4,10 @@ import '../styles/form.css';
 
 function Login(){
     const [data, setData] = useState({})
-    const [response, setResponse] = useState("");
+    const [response, setResponse] = useState({});
     const [token, setToken] = useState(false)
     useEffect(() => {
+        setResponse({text: "", status: false})
         setToken(getJWT())
     }, [])
     const handleSubmit = (e) => {
@@ -19,12 +20,15 @@ function Login(){
             }
         )
         .then(response => response.json())
-        .then(_data => {window.localStorage.setItem("token", _data); setResponse("Successfully logged in"); setTimeout(()=>{window.location.reload(true)}, 1500)})
-        .catch(err => setResponse(`An error occured while attempting to login in: ${err}`));
+        .then(_data => {window.localStorage.setItem("token", _data); setResponse({text: "Successfully logged in", status: true}); setTimeout(()=>{window.location.reload(true)}, 1500)})
+        .catch(err => setResponse({text: `Error: ${err}`, status: false}));
     }
     return (
         <>
-            <p>{response}</p>
+            <div className="form-action-header">
+                {response && response.text && <p className={`status-message span-two ${response.status ? "success" : "failure"}`}>{response.text || "Testing text vneovevioeveionveoiveonveonveoveoveovneo"}</p>}
+                <h1 className="form-title">Login</h1>
+            </div>
             {!token && <form className='form' onSubmit={handleSubmit}>
                 <input name="username" required className='form-text-input' placeholder='Username' onChange={(e) => setData({...data, ["username"] : e.target.value})}/>
                 <input name="password" required className='form-text-input' placeholder='Password' type="password" onChange={(e) => setData({...data, ["password"] : e.target.value})}/>
