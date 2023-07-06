f98740280fa2251dd54643e7fce9d8045b836887
David Sword <vexiconlieus@gmail.com>
2023-07-06 14:51:55 -0700

    Removed unneccessary parameters from callin Router

--- mac-app/src/pages/callInRouter.js
+++ mac-app/src/pages/callInRouter.js
@@ -5,29 +5,6 @@ import MyTableView from "../components/tableView";
 
 function CallInRouter(){
     const parameters = useParams();
-    const nav = useNavigate();
-    const [edit, setEdit] = useState(true);
-    const [confirm, setConfirm] = useState(false);
-    const toggle = () => {
-        if(edit){
-            setEdit(false);
-        }else{
-            setEdit(true);
-        }
-    }
-
-    async function deleteCustomer(){
-        await fetch("http://localhost:4000/callin/" + parameters['*'].split("/")[1], {
-            method: 'DELETE'}
-        ).then(response => {
-            if(response.ok){
-                setConfirm(false);
-                nav("/callin/");
-            }else{
-                console.log("failed");
-            }
-        }).catch(err => {console.log(err); console.log("failed");});
-    }
     return (
     <Routes>
         <Route path="/" element={
@@ -45,13 +22,11 @@ function CallInRouter(){
         <Route path="/add/" element={
         <CallInForm 
             method={"POST"}
-            edit={true}
             api={"http://localhost:4000/callin/"}/>}/>
         <Route path="/edit/:id" element={<>
             <CallInForm 
                 _id={parameters['*'].split("/")[1]}
                 method={"PUT"} 
-                _edit={edit}
                 api={"http://localhost:4000/callin/"}/></>}/>
     </Routes>
     )
