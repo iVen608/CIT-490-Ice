fbef06668f0883dcb005edc95ec739356db89582
David Sword <vexiconlieus@gmail.com>
2023-07-14 18:31:57 -0700

    Updated header bug

--- mac-app/src/App.js
+++ mac-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styles/App.css';
 import {Routes, Route} from 'react-router-dom';
-import Header from './components/Header';
+import Header from './components/header';
 import Footer from './components/Footer';
 import InvoiceCalculator from './pages/invoiceCalculator';
 import CustomerRouter from './pages/customerRouter';
