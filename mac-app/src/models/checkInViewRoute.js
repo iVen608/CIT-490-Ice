ade3ba1cfa1bbea95c78cdd41c7e9dfd35f50bec
David Sword <vexiconlieus@gmail.com>
2023-07-14 19:00:34 -0700

    Updated checkin update checkbox to be disabled

--- mac-app/src/models/checkInViewRoute.js
+++ mac-app/src/models/checkInViewRoute.js
@@ -21,18 +21,16 @@ function CheckInCustomer(props){
                 <td key={props._data._id+"delivered"} className='customer-table-data-cell'>
                     <input type="number" className='table-input' onChange={props.inputBox} placeholder={`${props._data.ice1}R`} value={props._savedData.delivered} readOnly={!delivered}/>
                     {props._data.price2 === 0 || props._data.price2 === '' ? <></> : <input type="number" className='table-input' onChange={props.inputBox2} value={props._savedData.delivered2} placeholder={`${props._data.ice2}R`} readOnly={!delivered}/>}</td>
-                <td>{}</td>
             </tr>)
     }else{
         return (
             <tr key={props._data._id} className='customer-table-data-row'>
-                <td key={props._data._id+"checkbox"} className='customer-table-data-cell'><input type="checkbox" onChange={(e) => {props.checkbox(e); setDelivered(e.target.checked)}}/></td>
+                <td key={props._data._id+"checkbox"} className='customer-table-data-cell'><input type="checkbox" checked={delivered} disabled={props._edit} onChange={(e) => {props.checkbox(e); setDelivered(e.target.checked)}}/></td>
                 <td key={props._data._id+props._data.name} className='customer-table-data-cell'>{props._data.name}</td>
                 <td key={props._data._id+"address"} className='customer-table-data-cell'>{props._data.address}</td>
                 <td key={props._data._id+"delivered"} className='customer-table-data-cell'>
                     <input type="number" className='table-input' onChange={props.inputBox} placeholder={`${props._data.ice1}R`} readOnly={!delivered}/>
                     {props._data.price2 === 0 || props._data.price2 === '' ? <></> : <input type="number" className='table-input' onChange={props.inputBox2} placeholder={`${props._data.ice2}R`} readOnly={!delivered}/>}</td>
-                <td>{}</td>
             </tr>)
     }
     
